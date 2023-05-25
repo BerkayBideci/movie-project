aa7cc017409bed8301145014e83103fb691c09ab
Medetcan Diler <medetdiler1996@gmail.com>
2023-05-25 18:49:07 +0300

    trying merge

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 'use strict';
 
-const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
-const PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185';
-const BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780';
-const CONTAINER = document.querySelector('.movie-container');
+const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
+const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
+const CONTAINER = document.querySelector(".movie-container");
 
 let movies = [];
 let genres = [];
 let currentFilter = 'now_playing'; // Initialize currentFilter with a default value
 
 
+
+// Don't touch this function please
 const autorun = async () => {
   await fetchMovies(currentFilter); // Pass the currentFilter as an argument
   await fetchGenres();
@@ -17,21 +19,19 @@ const autorun = async () => {
   renderGenres();
 };
 
+// Don't touch this function please
 const constructUrl = (path) => {
-  return `${TMDB_BASE_URL}/${path}?api_key=9c7643a821f2dd01a5781799a3e78f7c`;
+  return `${TMDB_BASE_URL}/${path}?api_key=${'9c7643a821f2dd01a5781799a3e78f7c'}`;
 };
 
+// You may need to add to this function, definitely don't delete it.
 const movieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
   renderMovie(movieRes);
 };
 
-const fetchMovie = async (movieId) => {
-  const url = constructUrl(`movie/${movieId}`);
-  const res = await fetch(url);
-  return res.json();
-};
 
+// This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
 const fetchMovies = async (filter) => {
   let url = '';
 
@@ -58,6 +58,13 @@ const fetchMovies = async (filter) => {
   movies = data.results;
 };
 
+// Don't touch this function please. This function is to fetch one movie.
+const fetchMovie = async (movieId) => {
+  const url = constructUrl(`movie/${movieId}`);
+  const res = await fetch(url);
+  return res.json();
+};
+
 const fetchGenres = async () => {
   const url = constructUrl('genre/movie/list');
   const res = await fetch(url);
@@ -66,6 +73,7 @@ const fetchGenres = async () => {
   return movies;
 };
 
+// You'll need to play with this function in order to add features and enhance the style.
 const renderMovies = (moviesToRender) => {
   const movieContainer = document.querySelector('.movie-container');
   movieContainer.innerHTML = '';
@@ -83,24 +91,163 @@ const renderMovies = (moviesToRender) => {
   });
 };
 
-const renderMovie = (movie) => {
+// You'll need to play with this function in order to add features and enhance the style.
+const renderMovie = async (movie) => {
+  console.log(movie)
+  let similar = await similarDetails(movie);
+  let slicedSimilar = similar.results.splice(0, 10);
+  let acts = await actorsDetails(movie);
+  let slicedActs = acts.cast.splice(0, 10)
+  let video = await videoDetails(movie)
+  console.log(video, "video")
+  const director = acts.crew.find(item => item.name)
   CONTAINER.innerHTML = `
-    <div class="row">
-      <div class="col-md-4">
-        <img id="movie-backdrop" src="${BACKDROP_BASE_URL + movie.backdrop_path}">
-      </div>
-      <div class="col-md-8">
-        <h2 id="movie-title">${movie.title}</h2>
-        <p id="movie-release-date"><b>Release Date:</b> ${movie.release_date}</p>
-        <p id="movie-runtime"><b>Runtime:</b> ${movie.runtime} Minutes</p>
-        <h3>Overview:</h3>
-        <p id="movie-overview">${movie.overview}</p>
-      </div>
-    </div>
-    <h3>Actors:</h3>
-    <ul id="actors" class="list-unstyled"></ul>
-  `;
+    <div class="container mx-auto ">
+        <div>
+             <img id="movie-backdrop" class="mx-auto rounded-lg" src=${BACKDROP_BASE_URL + movie.backdrop_path}>
+        </div>
+        <div class="">
+            <h1 id="movie-title" class='text-4xl font-bold text-center'>${movie.title}</h1>
+            <div class="text-center">
+            <h3 class='font-bold'>Vote Count</h3>
+            <p id="movie-release-date">${movie.vote_count}</p>
+            </div>
+            <div class="text-center">
+            <h3 class='font-bold'>Vote Average</h3>
+            <p id="movie-release-date">${movie.vote_average}</p>
+            </div>
+            <div class="text-center">
+            <h3 class='font-bold'>Release Date</h3>
+            <p id="movie-release-date">${movie.release_date}</p>
+            </div>
+            <div class="text-center">
+            <h3 class='font-bold'>Runtime</h3>
+            <p id="movie-runtime">${movie.runtime} Minutes</p>
+            </div>
+            <div class="text-center">
+            <h3 class='font-bold'>Language</h3>
+            <p> ${movie.original_language.toUpperCase()}  </p>
+            </div>
+            <div class="text-center director">
+            </div>
+            <div class="iframe">
+            </div>
+            <div class="text-center">
+            <h3 class='font-bold'>Overview</h3>
+            <p id="movie-overview">${movie.overview}</p>
+            </div>
+        </div>
+        </div>
+            <h3 class='text-3xl font-bold'>Actors:</h3>
+            <ul id="actors" class="container flex flex-wrap space-x-12   gap-y-6 py-6">
+            ${slicedActs.map(actor => `
+              <li class="actor">
+                <img width='100' height='100' class='rounded-full' src=${BACKDROP_BASE_URL + actor.profile_path} alt='${actor.name}'>
+                <p class=''> ${actor.name} </p>
+              </li>
+            `).join('')}
+            </ul>
+            <h3 class='text-3xl font-bold'>Similar Movies</h3>
+            <ul id="actors" class="container flex flex-wrap space-x-12  gap-y-6 py-6">
+                ${slicedSimilar.map(similar => `
+                <li class="similar-movie">
+                <img width='150' height='150' class='rounded-full' src=${BACKDROP_BASE_URL + similar.backdrop_path} alt='${similar.title}'>
+                <p class=''> ${similar.title} </p>
+              </li>
+                `).join('')}
+            </ul>
+            <h3 class='text-3xl font-bold'>Production Companies</h3>
+            <ul class="container flex flex-wrap space-x-12 gap-y-6 py-6 production-logo">
+            </ul>
+    </div>`;
+
+  const similarMovieDiv = document.querySelectorAll('.similar-movie');
+  similarMovieDiv.forEach((element, index) => {
+    element.addEventListener('click', () => {
+      movieDetails(slicedSimilar[index]);
+    });
+  })
+
+
+
+  const actorDiv = document.querySelectorAll('.actor');
+  actorDiv.forEach((element, index) => {
+    element.addEventListener('click', () => {
+      renderActor(slicedActs[index]);
+    });
+  })
+
+  const directorDiv = document.querySelector(".director")
+  if (director.name) {
+    directorDiv.innerHTML = `<h3 class='font-bold'>Director</h3>
+            <p> ${director.name} </p>`
+  }
+
+  const iframe = document.querySelector(".iframe")
+  if (video.results !== []) {
+    iframe.innerHTML = `<iframe width="560" height="315" class="mx-auto iframe" src="https://www.youtube.com/embed/${video.results[0].key}" title="YouTube video player" referrerpolicy="no-referrer-when-downgrade" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>`
+  }
+
+  const productionLogo = document.querySelector(".production-logo")
+  if (movie.production_companies[0].logo_path) {
+    productionLogo.innerHTML = `<li>
+                <img width='200' height='200'  src=${BACKDROP_BASE_URL + movie.production_companies[0].logo_path} alt='${movie.name}'>
+              </li>`
+  }
 };
+
+const renderActor = async (actor) => {
+  actor = await actorDetails(actor)
+  console.log(actor)
+  let movies = await fetchActorRelatedMovies(actor.id)
+  let slicedMovies = movies.cast.splice(0, 10)
+  let gender;
+  if (actor.gender === 1) {
+    gender = "Female"
+  } else if (actor.gender === 2) {
+    gender = "Male"
+  }
+  CONTAINER.innerHTML = `
+    <div class="container mx-auto ">
+        <div class="text-center">
+             <img width='200' height='200' class="rounded-full mx-auto" src=${BACKDROP_BASE_URL + actor.profile_path} alt='${actor.name}'>
+             <p id="movie-release-date"><b>${actor.name}</b></p>
+             <p><b>Gender</b></p>
+             <p>${gender}</p>
+             <ul id="movie-release-date"><b>Also Known As</b> ${actor.also_known_as.map(aka => `<li>${aka}</li>`).join("")}</ul>
+             <p><b>Popularity</b></p>
+             <p>${actor.popularity}</p>
+             <p id="movie-release-date"><b>Birthday:</b> ${actor.birthday}</p>
+             <div class="deathday"></div>
+             <p id="movie-release-date"><b>Place of Birth:</b> ${actor.place_of_birth}</p>
+             <p id="movie-release-date"><b>Biography:</b> ${actor.biography}</p>
+             <h3 class='text-3xl font-bold'>Related Movies</h3>
+             <ul class="container flex flex-wrap space-x-12 gap-y-6 py-6">
+                ${slicedMovies.map(related => `
+                <li class='related-movie'>
+                <img width='150' height='150' class='rounded-full' src=${BACKDROP_BASE_URL + related.backdrop_path} alt='${related.title}'>
+                <p class=''> ${related.title} </p>
+              </li>
+                `).join('')}
+            </ul>
+        </div>`
+  const relatedMovieImages = document.querySelectorAll('.related-movie');
+  relatedMovieImages.forEach((image, index) => {
+    image.addEventListener('click', () => {
+      movieDetails(slicedMovies[index]);
+    });
+  })
+
+  const deathday = document.querySelector(".deathday")
+  if (actor.deathday !== null) {
+    deathday.innerHTML = `<p><b>Deathday:</b> ${actor.deathday}</p>`
+  }
+};
+
+
+document.addEventListener("DOMContentLoaded", autorun);
+
 
 const renderGenres = () => {
   const genreDropdown = document.querySelector('#genre-dropdown');
@@ -231,26 +378,64 @@ filterMenu.forEach(menu => {
   });
 });
 
-document.addEventListener("DOMContentLoaded", autorun);
-
-// ACTOR PAGE
-const actorPage = document.querySelector('#actor');
 
 
-const renderActorPage = async (movie) => {
-  console.log(movie)
+// fetching movie video details 
+const fetchMovieVideo = async (movie) => {
+  const url = constructUrl(`movie/${movie}/videos`)
+  const res = await fetch(url)
+  return res.json()
 }
 
-const actorPageDetails = async (movie) => {
-  const actorRes = await fetchActorPage(movie.id);
-  renderActorPage(actorRes);
+const videoDetails = async (movie) => {
+  const movieRes = await fetchMovieVideo(movie.id)
+  return movieRes
 }
 
-const fetchActorPage = async (actor) => {
+// fetching actors' details 
+const fetchActorDetails = async (actor) => {
+  const url = constructUrl(`person/${actor}`)
+  const res = await fetch(url)
+  return res.json()
+}
+
+const actorDetails = async (actor) => {
+  const actorRes = await fetchActorDetails(actor.id)
+  return actorRes
+}
+
+// fetching actor's related movies
+const fetchActorRelatedMovies = async (actor) => {
+  const url = constructUrl(`person/${actor}/combined_credits`)
+  const res = await fetch(url)
+  return res.json()
+}
+
+const actorRelatedMovieDetails = async (actor) => {
+  const actorRelatedMoviesRes = await fetchActorRelatedMovies(actor.id)
+  return actorRelatedMoviesRes
+}
+
+// fetching actors
+const fetchActors = async (actor) => {
   const url = constructUrl(`movie/${actor}/credits`);
   const res = await fetch(url);
   return res.json();
 }
 
+const actorsDetails = async (movie) => {
+  const actorRes = await fetchActors(movie.id);
+  return actorRes;
+};
 
+// fetching similar movies
+const fetchSimilar = async (similar) => {
+  const url = constructUrl(`/movie/${similar}/similar`);
+  const res = await fetch(url);
+  return res.json();
+};
 
+const similarDetails = async (movie) => {
+  const similarRes = await fetchSimilar(movie.id);
+  return similarRes
+}
