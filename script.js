41de5418c84bedfba1eba0d150befd78b3d042e4
Medetcan Diler <medetdiler1996@gmail.com>
2023-05-27 15:44:07 +0300

    About page style, actor page style added

--- script.js
+++ script.js
@@ -84,10 +84,10 @@ const renderMovies = (moviesToRender) => {
     const movieDiv = document.createElement('div');
     movieDiv.classList.add('cursor-pointer');
     movieDiv.innerHTML = `
-      <img class='transition opacity-80 hover:opacity-100 ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' height='400' width='400' src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title} poster">
+      <img class='transition shadow-xl shadow-slate-900 opacity-80 hover:opacity-100 ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' height='400' width='400' src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title} poster">
       <div class='space-y-2 pt-3'>
-        <h3 class='text-xl font-bold'>${movie.title}</h3>
-        <p class='text-xs text-slate-400'>Genres: ${getGenresString(movie.genre_ids)}</p>
+        <h3 class='text-2xl font-bold'>${movie.title}</h3>
+        <p class='text-sm text-slate-400'>Genres: ${getGenresString(movie.genre_ids)}</p>
       </div>
       `;
     movieDiv.addEventListener('click', async () => {
@@ -116,41 +116,41 @@ const renderMovie = async (movie) => {
     </div>
     <h1 id="movie-title" class='text-4xl font-bold text-center py-3 lg:text-6xl'>${movie.title}</h1>
     <div class="container flex flex-col space-y-12   md:flex-row md:space-x-12 md:space-y-0">
-      <div class="left w-1/2 space-y-4">  
-          <h1 id="movie-title" class='text-3xl font-bold text-center'>Preface</h1>
-          <div class='flex space-x-3 pt-7 justify-center  '>
-            <h3 class='font-bold '>Vote Count:</h3>
-            <p id="movie-release-date">${movie.vote_count}</p>
+      <div class="left w-1/2 space-y-4 text-xl">  
+          <h1 id="movie-title" class='text-4xl font-bold text-center'>Preface</h1>
+          <div class='flex  space-x-3 pt-7 justify-center  '>
+            <h3 class='font-bold '>Vote Count</h3>
+            <p id="movie-release-date" class='text-slate-400'>${movie.vote_count}</p>
           </div>
           <div class='flex space-x-3 justify-center'>
             <h3 class='font-bold '>Vote Average</h3>
-            <p id="movie-release-date">${movie.vote_average}</p>
+            <p id="movie-release-date" class='text-slate-400'>${movie.vote_average}</p>
           </div>
           <div class='flex space-x-3 justify-center'>
             <h3 class='font-bold'>Release Date</h3>
-            <p id="movie-release-date">${movie.release_date}</p>
+            <p id="movie-release-date" class='text-slate-400'>${movie.release_date}</p>
           </div>
           <div class='flex space-x-3 justify-center'>
             <h3 class='font-bold'>Runtime</h3>
-            <p id="movie-runtime">${movie.runtime} Minutes</p>
+            <p id="movie-runtime" class='text-slate-400'>${movie.runtime} Minutes</p>
             </div>
           <div class='flex space-x-3 justify-center '>
             <h3 class='font-bold'>Language</h3>
-            <p> ${movie.original_language.toUpperCase()}  </p>
+            <p class='text-slate-400'>  ${movie.original_language.toUpperCase()}  </p>
           </div>
           <div class='flex space-x-3 justify-center '>
             <h3 class='font-bold'>Director:</h3>
-            <p> ${director.name} </p>
+            <p class='text-slate-400'> ${director.name} </p>
           </div>
       </div>
       
       <div class="right w-1/2 text-center">
-        <h1 class='text-3xl font-bold pb-4 text-center'>Trailer</h1>
+        <h1 class='text-4xl font-bold pb-4 text-center'>Trailer</h1>
         <div>
         ${
           trailer.results && trailer.results.length > 0 && trailer.results[0].key
-            ? `<div>
-                 <iframe width="88%" height="275" src="https://www.youtube.com/embed/${trailer.results[0].key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            ? `<div class='pl-6'>
+                 <iframe class='ml-6 shadow-xl shadow-slate-900' width="83%" height="275" src="https://www.youtube.com/embed/${trailer.results[0].key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                  <figcaption class="overlay"></figcaption>
                </div>`
             : `<p>No trailer available</p>`
@@ -161,33 +161,33 @@ const renderMovie = async (movie) => {
 
     <div class='space-y-3 pt-12 text-center'>
         <div id="Actors">
-          <h3 class='text-3xl font-bold'>Actors:</h3>
+          <h3 class='text-4xl font-bold'>Actors</h3>
           <ul id="actors" class="container flex flex-wrap space-x-12 justify-center gap-y-6 py-6">
           ${slicedActs.map(actor => `
             <li class='cursor-pointer actor'>
-              <img width='150' height='100' class='rounded-full opacity-80 hover:opacity-100 rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src=${BACKDROP_BASE_URL + actor.profile_path} alt='${actor.name}'>
-              <p class='font-bold pt-2 text-slate-500'> ${actor.name} </p>
+              <img width='150' height='100' class='rounded-full shadow-xl shadow-slate-900 opacity-80 hover:opacity-100 rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src=${BACKDROP_BASE_URL + actor.profile_path} alt='${actor.name}'>
+              <p class='font-bold pt-2 text-slate-400'> ${actor.name} </p>
             </li>
           `).join('')}
           </ul>
         </div>
         <div id="relatedMovies">
-          <h3 class='text-3xl font-bold'>Similar Movies</h3>
+          <h3 class='text-4xl font-bold'>Similar Movies</h3>
           <ul id="actors" class="container flex flex-wrap space-x-12 justify-center gap-y-6 py-6">
               ${slicedSimilar.map(similar => `
             <li class='cursor-pointer similar-movie'>
-              <img width='300' height='300' class='rounded-full opacity-80 hover:opacity-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src=${BACKDROP_BASE_URL + similar.backdrop_path} alt='${similar.title}'>
-              <p class='font-bold pt-2 text-slate-500'> ${similar.title} </p>
+              <img width='300' height='300' class='rounded-full shadow-xl shadow-slate-900 opacity-80 hover:opacity-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src=${BACKDROP_BASE_URL + similar.backdrop_path} alt='${similar.title}'>
+              <p class='font-bold pt-2 text-slate-400'> ${similar.title} </p>
             </li>
               `).join('')}
           </ul>
         </div>
         <div id="relatedMovies">
-          <h3 class='text-3xl font-bold'>Production Companies</h3>
+          <h3 class='text-4xl font-bold'>Production Companies</h3>
           <ul id="actors" class="container flex flex-wrap space-x-12 justify-center gap-y-6 py-6">
               ${`
             <li class='cursor-pointer'>
-              <img width='200' height='200' class='transition opacity-80 hover:opacity-100 ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src=${BACKDROP_BASE_URL + movie.production_companies[0].logo_path} alt='${movie.name}'>
+              <img width='200' height='200' class='transition  opacity-80 hover:opacity-100 ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src=${BACKDROP_BASE_URL + movie.production_companies[0].logo_path} alt='${movie.name}'>
             </li>
               `}
           </ul>
@@ -228,7 +228,7 @@ const trailerDetails = (movie) => {
 const renderActor = async (actor) => {
   actor = await actorDetails(actor)
   let movies = await fetchActorRelatedMovies(actor.id)
-  let slicedMovies = movies.cast.splice(0, 10)
+  let slicedMovies = movies.cast.splice(0, 5)
   let gender;
   if (actor.gender === 1) {
     gender = "Female"
@@ -237,24 +237,46 @@ const renderActor = async (actor) => {
   }
   CONTAINER.innerHTML = `
     <div class="container mx-auto ">
-        <div class="text-center">
-             <img width='200' height='200' class="rounded-full mx-auto" src=${BACKDROP_BASE_URL + actor.profile_path} alt='${actor.name}'>
-             <p id="movie-release-date"><b>${actor.name}</b></p>
-             <p><b>Gender</b></p>
-             <p>${gender}</p>
-             <ul id="movie-release-date"><b>Also Known As</b> ${actor.also_known_as.map(aka => `<li>${aka}</li>`).join("")}</ul>
-             <p><b>Popularity</b></p>
-             <p>${actor.popularity}</p>
-             <p id="movie-release-date"><b>Birthday:</b> ${actor.birthday}</p>
+        <div class="flex flex-col space-y-2 items-center text-center text-xl">
+             <img width='200' height='200' class="rounded-full shadow-xl shadow-slate-900 mx-auto" src=${BACKDROP_BASE_URL + actor.profile_path} alt='${actor.name}'>
+             <h3 id="movie-release-date" class='text-6xl py-2 font-bold'><b>${actor.name}</b></h3>
+             <div class='flex space-x-3 justify-center'>
+              <h3 class='font-bold'>Gender</h3>
+              <p class='text-slate-400'>${gender}</p>
+             </div>
+
+             <div class='flex space-x-3 justify-center'>
+              <h3 class='font-bold'>Also Known As</h3>
+              <p class='text-slate-400'>${actor.also_known_as[0]}</p>
+             </div>
+
+             <div class='flex space-x-3 justify-center'>
+              <h3 class='font-bold'>Popularity</h3>
+              <p class='text-slate-400'>${actor.popularity}</p>
+             </div>
+
+             <div class='flex space-x-3 justify-center'>
+              <h3 class='font-bold'>Birthday</h3>
+              <p class='text-slate-400'>${actor.birthday}</p>
+             </div>
+
              <div class="deathday"></div>
-             <p id="movie-release-date"><b>Place of Birth:</b> ${actor.place_of_birth}</p>
-             <p id="movie-release-date"><b>Biography:</b> ${actor.biography}</p>
-             <h3 class='text-3xl font-bold'>Related Movies</h3>
-             <ul class="container flex flex-wrap space-x-12 gap-y-6 py-6">
+
+             <div class='flex space-x-3 justify-center'>
+              <h3 class='font-bold'>Place of Birth</h3>
+              <p class='text-slate-400'>${actor.place_of_birth}</p>
+             </div>
+
+             <div class='flex space-x-3 justify-center w-1/2 '>
+              <h3 class='font-bold'>Biography</h3>
+              <p class='text-slate-400'>${actor.biography}</p>
+             </div>
+             <h3 class='text-3xl font-bold py-3'>Related Movies</h3>
+             <ul class="container flex flex-wrap cursor-pointer space-x-12 justify-center gap-y-6 py-6">
                 ${slicedMovies.map(related => `
                 <li class='related-movie'>
-                <img width='150' height='150' class='rounded-full' src=${BACKDROP_BASE_URL + related.backdrop_path} alt='${related.title}'>
-                <p class=''> ${related.title} </p>
+                <img width='300' height='300' class='rounded-full shadow-xl shadow-slate-900 opacity-80 hover:opacity-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src=${BACKDROP_BASE_URL + related.backdrop_path} alt='${related.title}'>
+                <p class='pt-2 font-bold text-slate-400'> ${related.title} </p>
               </li>
                 `).join('')}
             </ul>
@@ -268,7 +290,10 @@ const renderActor = async (actor) => {
 
   const deathday = document.querySelector(".deathday")
   if (actor.deathday !== null) {
-    deathday.innerHTML = `<p><b>Deathday:</b> ${actor.deathday}</p>`
+    deathday.innerHTML = `
+    <h3 class='font-bold'>Deathday</h3>
+    <p class='text-slate-400'>${actor.deathday}</p>
+    `
   }
 };
 
@@ -478,7 +503,7 @@ actorPage.forEach(actorPage => actorPage.addEventListener('click', async (e) => 
   ${actors.results.map(actor => {
     return `
     
-    <div class='single-actor  flex flex-col  space-y-4 cursor-pointer'> <img class='rounded-full opacity-75 hover:opacity-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' width='200' height='200' src=${BACKDROP_BASE_URL + actor.profile_path} alt=${actor.name}>
+    <div class='single-actor  flex flex-col  space-y-4 cursor-pointer'> <img class='rounded-full shadow-xl shadow-slate-800 opacity-75 hover:opacity-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' width='200' height='200' src=${BACKDROP_BASE_URL + actor.profile_path} alt=${actor.name}>
       <p>${actor.name}</p>
     </div>
     `
@@ -519,7 +544,7 @@ function displayFilteredMovies(movies) {
       <img class='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' height='400' width='400' src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title} poster">
       <div class='space-y-2 pt-3'>
         <h3 class='text-xl font-bold'>${movie.title}</h3>
-        <p class='text-xs text-slate-500'>Genres: ${getGenresString(movie.genre_ids)}</p>
+        <p class='text-xs text-slate-400'>Genres: ${getGenresString(movie.genre_ids)}</p>
       </div>
       `;
     movieDiv.addEventListener('click', async () => {
@@ -534,7 +559,7 @@ function displayFilteredMovies(movies) {
 
 async function filterMoviesByInput() {
   const movies = await fetchMovies('now_playing');
-  
+
   console.log(movies)
   const searchValue = searchInput.value.toLowerCase()
 
@@ -550,11 +575,9 @@ filterMoviesByInput()
 // ABOUT PAGE
 // script.js
 const aboutContent = `
-<h1 class="about-heading">nice to meet you!!</h1>
-<div class="iframe-container">
-  <iframe src="https://giphy.com/embed/1ZVBVvY5kS7qUHhqI2" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-</div>
-  <h2 class="about-subheading">and bye!</h2>
+<h3 class="about-heading text-4xl">nice to meet you!!</h3>
+<img src="./media/ZIb4.gif" alt="gifforabout">
+<h3 class="about-subheading text-4xl">and bye!</h3>
 
 `;
 
